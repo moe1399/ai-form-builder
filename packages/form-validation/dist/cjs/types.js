0a194cc1131f573a77535b8c13b3c2ff9fd521f9
dan <dan@nomail.com>
2026-01-08 13:27:24 +1300

    Add build artifacts to .gitignore and remove from tracking

--- packages/form-validation/dist/cjs/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Types for @moe1399/form-validation
- * These mirror the types from @moe1399/ngx-dynamic-forms
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- packages/form-validation/dist/esm/types.js
+++
@@ -1,5 +0,0 @@
-/**
- * Types for @moe1399/form-validation
- * These mirror the types from @moe1399/ngx-dynamic-forms
- */
-export {};
